ce37be26a4ec3b4945677c9095f7e2cfc707e82c
Dennis <dennis@tryitonline.net>
2018-02-19 01:13:24 +0000

    fixed NOPQR instructions; tweaked for non-interactive use

--- AB.cpp
+++ AB.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 int Num_char = 0;
 int Position = 0;
+int NewPosition = 0;
 
 int Character;
 string Program[10000];
@@ -24,7 +25,7 @@ int main(int argc, char *argv[])
 {
     FILE * FileInd;
     
-    FileInd = fopen("Test.ab","r");
+    FileInd = fopen(argv[1],"r");
     
     do {
         Character = getc(FileInd);
@@ -36,6 +37,8 @@ int main(int argc, char *argv[])
     fclose(FileInd);
     
     do {
+        NewPosition = Position;
+
         if ( Program[Position] == "a" ) { Register1 += 1 ;}
         if ( Program[Position] == "b" ) { Register1 -= 1 ;}
         if ( Program[Position] == "c" ) { Register1 += 10 ;}
@@ -76,11 +79,11 @@ int main(int argc, char *argv[])
         if ( Program[Position] == "K" ) { if (( Temp = getchar()) != EOF ) Register2 = Temp ;}
         if ( Program[Position] == "L" ) { cout << char(Register3) ;}
         if ( Program[Position] == "M" ) { cout << int(Register3) ;}
-        if ( Program[Position] == "N" ) { if ( Register1 == Register2 ) Position = Register4[1] ;}
-        if ( Program[Position] == "O" ) { if ( Register1 != Register2 ) Position = Register4[1] ;}
-        if ( Program[Position] == "P" ) { if ( Register1 >= Register2 ) Position = Register4[1] ;}
-        if ( Program[Position] == "Q" ) { if ( Register1 <= Register2 ) Position = Register4[1] ;}
-        if ( Program[Position] == "R" ) { if ( Register3 == 0 ) Position = Register4[1] ;}
+        if ( Program[Position] == "N" ) { if ( Register1 == Register2 ) NewPosition = Register4[1];}
+        if ( Program[Position] == "O" ) { if ( Register1 != Register2 ) NewPosition = Register4[1];}
+        if ( Program[Position] == "P" ) { if ( Register1 >= Register2 ) NewPosition = Register4[1];}
+        if ( Program[Position] == "Q" ) { if ( Register1 <= Register2 ) NewPosition = Register4[1];}
+        if ( Program[Position] == "R" ) { if ( Register3 == 0 ) NewPosition = Register4[1] - 1;}
         if ( Program[Position] == "S" ) { Register4[Mode] += 1 ;}
         if ( Program[Position] == "T" ) { Register4[Mode] -= 1 ;}
         if ( Program[Position] == "U" ) { Register4[Mode] += 10 ;}
@@ -90,13 +93,8 @@ int main(int argc, char *argv[])
         if ( Program[Position] == "Y" ) { Register4[Mode] = 0 ;}
         if ( Program[Position] == "Z" ) { Mode = ( Mode + 1 ) % 2 ;}
         
-        
-        
-        Position ++;
+        Position = (Position == NewPosition) ? Position + 1 : NewPosition;
         } while ( Position <= Num_char );
         
-    cout << "\n\n";
-    
-    system("PAUSE");
     return ( 1 );
 }
